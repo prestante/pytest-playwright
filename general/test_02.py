05ee0634ca7c9a1e2aedcc974e67a4d7af2d0076
Alexander Galkovsky <prestante@yandex.ru>
2024-12-05 17:02:41 +0100

    Learning fixtures and their scopes

--- general/test_02.py
+++ general/test_02.py
@@ -6,23 +6,15 @@ import re
 import pytest
 from playwright.sync_api import Page, expect
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(autouse=True)
 def before_each_after_each(page: Page):
-    
-    # print("before the test runs")
-
-    # Go to the starting url before each test.
+    # Go to the starting url before each test for this module (each function of the module will have its own fresh page object with playwright.dev).
     page.goto("https://playwright.dev/")
-    yield
     
-    # print("after the test runs")
-
 def test_main_navigation(page: Page):
     # Assertions use the expect API.
-    expect(page).to_have_url("https://playwright.dev/")
+    expect(page).to_have_url("https://playwright.dev/", timeout=100)
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
-
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("playwright", re.IGNORECASE))
+    expect(page).to_have_title(re.compile("playwright", re.IGNORECASE), timeout=100)
