7686af36006ae3ca07f18e5fdcc95ff1990590bf
Alexander Galkovsky <prestante@yandex.ru>
2024-12-05 12:11:28 +0100

    Moved logged_page fixture to conftest so that all modules can use it

--- general/conftest.py
+++ general/conftest.py
@@ -1,7 +1,17 @@
 import pytest
+from playwright.sync_api import Page, expect
 
 @pytest.fixture(autouse=True, scope='package')
 def log_test():
     print("\nTest is starting")
     yield
     print("\nTest is ending")
+
+@pytest.fixture()
+def logged_page(page: Page):
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"login-button\"]").click()
+    expect(page.locator("[data-test=\"title\"]")).to_contain_text("Products")
+    return page
