af98e5ffe61dd1fc65568366da5a5ba4349da5a9
Alexander Galkovsky <prestante@yandex.ru>
2024-12-06 14:49:09 +0100

    added empty requirements.txt

--- yaml_parser.py
+++ yaml_parser.py
@@ -2,7 +2,7 @@ import yaml
 import json
 
 if __name__ == "__main__":
-    with open("./test.yaml") as f:
+    with open("/Users/pres/PY/pytest-playwright/.github/workflows/playwright.yml") as f:
         templates = yaml.safe_load(f)
     
     #print(templates)
